7d76301745922538339f14757d0861d19bfbaf54
alexey <ax.bratsun@gmail.com>
2023-08-26 22:29:32 +0500

    Поменял название модели

--- testProject/app/Http/Controllers/RequestsController.php
+++ testProject/app/Http/Controllers/RequestsController.php
@@ -5,23 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Resources\RequestsCollection;
 use App\Http\Requests\RequestsValidation;
-use App\Models\RequestsModel;
+use App\Models\RequestsStoreModel;
 use App\Enums\RequestsEnum;
 
 class RequestsController extends Controller
 {
     // GET
     public function index() {
-        return new RequestsCollection(RequestsModel::all());
+        return new RequestsCollection(RequestsStoreModel::all());
     }
 
     // POST
     public function store(RequestsValidation $request) {
-        return response()->json(RequestsModel::create(array_merge($request->all(), ['status' => RequestsEnum::Active])));
+        return response()->json(RequestsStoreModel::create(array_merge($request->all(), ['status' => RequestsEnum::Active])));
     }
 
     // PUT
     public function update(RequestsValidation $request) {
-        return Address::update($request->all());
+        return response()->json(RequestsStoreModel::update($request->all()));
     }
 }
--- testProject/app/Models/RequestsStoreModel.php
+++ testProject/app/Models/RequestsStoreModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class RequestsModel extends Model
+class RequestsStoreModel extends Model
 {
     protected $table = 'requests';
 
