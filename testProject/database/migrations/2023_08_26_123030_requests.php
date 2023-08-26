87661990ddd690fa4b8f9003d756f708fbb50172
alexey <ax.bratsun@gmail.com>
2023-08-26 18:16:47 +0500

    Миграция БД requests

---
+++ testProject/database/migrations/2023_08_26_123030_requests.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Requests extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    protected $connection = 'mysql';
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('email', 50);
+            $table->enum('status', ['Active', 'Resolved']);
+            $table->string('message', 2000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    // public function down()
+    // {
+    //     Schema::drop('requests');
+    // }
+}
